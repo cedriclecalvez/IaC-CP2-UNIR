28c726f25d071fc7e013e3a04fb59c5c0a92b895
Le <cedric_lecalvez@hotmail.com>
2025-03-11 19:51:17 +0100

    fix errors of modules

--- terraform/inventory_and_vars.tf
+++ terraform/inventory_and_vars.tf
@@ -1,15 +1,15 @@
 # Create dynamic inventory file for Ansible
 resource "local_file" "ansible_inventory" {
   depends_on = [
-    azurerm_linux_virtual_machine.vm,
-    azurerm_container_registry.acr,
-    azurerm_kubernetes_cluster.aks
+    module.vm,
+    module.acr,
+    module.aks
   ]
   content = templatefile("inventory_hosts_dynamic.tmpl",
     {
-      vm_public_ip_address = azurerm_linux_virtual_machine.vm.vm_ip_address
-      vm_user              = azurerm_linux_virtual_machine.vm.admin_username
-      aks_cluster_name     = azurerm_kubernetes_cluster.aks.name
+      vm_public_ip_address = module.vm.vm_public_ip_addresses[0]
+      vm_user              = "adminuser"
+      aks_cluster_name     = module.aks.aks_cluster_name
     }
   )
   filename = "inventory_hosts_dynamic"
@@ -18,19 +18,19 @@ resource "local_file" "ansible_inventory" {
 # Create dynamic all_group_vars_secrets file for Ansible
 resource "local_file" "ansible_all_group_vars_secrets" {
   depends_on = [
-    azurerm_linux_virtual_machine.vm,
-    azurerm_container_registry.acr,
-    azurerm_kubernetes_cluster.aks
+    module.vm,
+    module.acr,
+    module.aks
   ]
   content = templatefile("inventory_vars_dynamic.tmpl",
     {
-      vm_public_ip_address = azurerm_linux_virtual_machine.vm.vm_ip_address
-      acr_login_server     = azurerm_container_registry.acr.login_server
-      acr_username         = azurerm_container_registry.acr.admin_username
-      acr_password         = azurerm_container_registry.acr.admin_password
-      acr_username         = azurerm_container_registry.acr.admin_username
-      aks_cluster_name     = azurerm_kubernetes_cluster.aks.name
-      aks_kube_config      = azurerm_kubernetes_cluster.aks.kube_config_raw
+      vm_public_ip_address = module.vm.vm_public_ip_addresses[0]
+      vm_user              = "adminuser" # Add this line
+      acr_login_server     = module.acr.acr_login_server
+      acr_username         = module.acr.acr_username
+      acr_password         = module.acr.acr_password
+      aks_cluster_name     = module.aks.aks_cluster_name
+      aks_kube_config      = module.aks.aks_kube_config
     }
   )
   filename = "inventory_vars_dynamic.yml"
