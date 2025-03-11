4405ef3c268ad5ba001de7fa9ebb9c397f02d5f7
Le <cedric_lecalvez@hotmail.com>
2025-03-11 19:36:01 +0100

    update inventory

--- terraform/inventory_and_vars.tf
+++ terraform/inventory_and_vars.tf
@@ -5,14 +5,14 @@ resource "local_file" "ansible_inventory" {
     azurerm_container_registry.acr,
     azurerm_kubernetes_cluster.aks
   ]
-  content = templatefile("inventory_hosts.tmpl",
+  content = templatefile("inventory_hosts_dynamic.tmpl",
     {
       vm_public_ip_address = azurerm_linux_virtual_machine.vm.vm_ip_address
       vm_user              = azurerm_linux_virtual_machine.vm.admin_username
       aks_cluster_name     = azurerm_kubernetes_cluster.aks.name
     }
   )
-  filename = "inventory_hosts"
+  filename = "inventory_hosts_dynamic"
 }
 
 # Create dynamic all_group_vars_secrets file for Ansible
@@ -22,7 +22,7 @@ resource "local_file" "ansible_all_group_vars_secrets" {
     azurerm_container_registry.acr,
     azurerm_kubernetes_cluster.aks
   ]
-  content = templatefile("inventory_all_group_vars_secrets.tmpl",
+  content = templatefile("inventory_vars_dynamic.tmpl",
     {
       vm_public_ip_address = azurerm_linux_virtual_machine.vm.vm_ip_address
       acr_login_server     = azurerm_container_registry.acr.login_server
@@ -33,5 +33,5 @@ resource "local_file" "ansible_all_group_vars_secrets" {
       aks_kube_config      = azurerm_kubernetes_cluster.aks.kube_config_raw
     }
   )
-  filename = "inventory_all_group_vars_secrets.yml"
+  filename = "inventory_vars_dynamic.yml"
 }
