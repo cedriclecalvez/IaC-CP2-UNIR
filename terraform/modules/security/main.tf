dc647063a1f5a3c711f441aab415bc7a8bf005de
Le <cedric_lecalvez@hotmail.com>
2025-03-20 22:35:54 +0100

    change url back including dns

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -26,6 +26,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "Backend-Port-8080"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
   tags = {
     environment = var.tag_value
   }
